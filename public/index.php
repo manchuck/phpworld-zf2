d19f70c542108f11d02ca331496a69fe5dd38754
Chuck "MANCHUCK" Reeves <chuck@manchuck.com>
2015-10-31 13:35:38 -0400

    Updated index.php to allow for development config

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php // @codingStandardsIgnoreFile
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
@@ -6,16 +6,31 @@
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server') {
-    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    if (__FILE__ !== $path && is_file($path)) {
-        return false;
-    }
-    unset($path);
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
+// Make sure composer was run
+if (!file_exists('vendor/autoload.php')) {
+    throw new RuntimeException(
+        'Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.'
+    );
 }
 
 // Setup autoloading
-require 'init_autoloader.php';
+include 'vendor/autoload.php';
+
+if (!defined('APPLICATION_PATH')) {
+    define('APPLICATION_PATH', realpath(__DIR__ . '/../'));
+}
+
+// Load the main config
+$appConfig = include APPLICATION_PATH . '/config/application.config.php';
+
+// Load different modules/settings during development
+if (file_exists(APPLICATION_PATH . '/config/development.config.php')) {
+    $appConfig = Zend\Stdlib\ArrayUtils::merge($appConfig, include APPLICATION_PATH . '/config/development.config.php');
+}
 
 // Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+Zend\Mvc\Application::init($appConfig)->run();
